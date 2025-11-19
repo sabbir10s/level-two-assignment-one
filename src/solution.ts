a72ac32dc57c5f61a8bcd32a3f3c68eff8b64bfe
sabbir10s <sabbirahmed1023@gmail.com>
2025-11-19 20:48:16 +0600

    problem 5 solved and problem 4 return type corrected

--- src/solution.ts
+++ src/solution.ts
@@ -32,14 +32,34 @@ type Books = {
   rating: number;
 };
 
-const filterByRating = (books: Books[]) => {
+const filterByRating = (books: Books[]): Books[] => {
   return books.filter((book) => book.rating >= 4);
 };
 
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 },
+// ];
+
+// console.log(filterByRating(books));
+
+type Users = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+const filterActiveUsers = (users: Users[]): Users[] => {
+  return users.filter((user) => user.isActive);
+};
+
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
 ];
 
-console.log(filterByRating(books));
+// console.log(filterActiveUsers(users));
+
+// console.log(users);
